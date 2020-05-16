f003488a8416a16b568eba9ed1b39d95b5793c4c
brian <brianbb174@protonmail.com>
2020-05-16 11:05:38 -0700

    add alt method

--- scream.py
+++ scream.py
@@ -10,3 +10,12 @@ with open("screaming_fighter.csv", "w") as file:
     csv_writer = writer(file)
     for fighter in fighters:
         csv_writer.writerow(fighter)
+
+# alternate method
+
+with open('fighters.csv') as file:
+    csv_reader = reader(file)
+    with open("screaming_fighter.csv", "w") as file:
+    csv_writer = writer(file)
+        for fighter in csv_reader:
+        csv_reader.writerow([s.upper() for s in fighter])
