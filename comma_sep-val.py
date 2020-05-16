5ea0deed791b266215c279d12969db7c14296e8c
brian <brianbb174@protonmail.com>
2020-05-15 18:14:31 -0700

    add dictreader

--- comma_sep-val.py
+++ comma_sep-val.py
@@ -9,6 +9,13 @@
 from csv import reader
 with open("fighter.csv")as file:
     csv_reader = reader(file)
-    for row in csv_reader:
-        print(row)
+    next(csv_reader)
+    for fighter in csv_reader:
+        print(f"{fighter[0]} is from {fighter[1]}")
 
+from csv import DictReader
+with open ("fighter.csv") as file:
+    csv_reader = DictReader(file)
+    next(csv_reader)
+    for fighter in csv_reader:
+        print(fighter['Name'])
